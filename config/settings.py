c2e0d358856a33174f9c0b42275463cab2589068
James Horan <horan5034@gmail.com>
2018-02-12 21:06:40 +0000

    adding database_url

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-#import dj_database_url
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -115,12 +115,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    #'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 
-'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    } 
+# 'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     } 
     #mysql:// b54acab45397da: dd4af3ca@eu-cdbr-west-02.cleardb.net/heroku_325286721bfcfb7?reconnect=true
 }
 AUTHENTICATION_BACKENDS = (
